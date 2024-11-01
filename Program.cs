919e9c7b73251dd044730143879dbf0fc29767e5
david <david@DESKTOP-SG8VE62>
2024-11-01 09:36:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace TC_Parkering_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string användarval;
+            Console.WriteLine("Välj din roll");
+            Console.WriteLine("1: Kund");
+            Console.WriteLine("2: Parkeringsvakt");
+            Console.WriteLine("3: Ägare");
+            användarval = Console.ReadLine();
+            switch (användarval)
+            {
+                case "1":
+                    Kund();
+                    break;
+
+                case "2":
+                    Vakt();
+                    break;
+
+                case "3":
+                    Ägare();
+                    break;
+
+                default:
+                    Console.WriteLine("Ogiltigt");
+                    break;
+            }
+            static void Kund()
+            {
+                Console.WriteLine("Kund");
+            }
+
+
+            static void Vakt()
+            {
+                Console.WriteLine("Vakt");
+            }
+
+
+            static void Ägare()
+            {
+                Console.WriteLine("Ägare");
+            }
+        }
+    }
+}
